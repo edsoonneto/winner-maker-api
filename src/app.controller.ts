c3b3fbe4db274f451c4efac7aac739509e3e0e01
Edson Neto <edson@upplify.com>
2020-09-04 22:12:38 -0300

    Update: Included new imports on controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get()
+    @Get('auth') // HTTP GET - localhost:3000/auth
     public getHello(): string {
         return this.appService.getHello();
     }
